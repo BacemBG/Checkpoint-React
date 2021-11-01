c2d4e7097ae649b6ecd1abcc90125da9037fd995
Bacembougda <89839441+BacemBG@users.noreply.github.com>
2021-11-01 03:21:05 +0000

    first commit

---
+++ ws-react/src/App.js
@@ -0,0 +1,14 @@
+import Form from "./Components/Form"
+import './App.css';
+import CardProfil from "./Components/CardProfil";
+
+function App() {
+  return (
+    <div className="App">
+     <Form />
+     <CardProfil />
+    </div>
+  );
+}
+
+export default App;
